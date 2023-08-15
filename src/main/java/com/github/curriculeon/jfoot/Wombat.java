9a94459cdfa5bcc2ae9b410c982c25760957a485
JLugo4 <jarek.lugo96@gmail.com>
2023-08-15 19:10:13 -0400

    Working on wombat

--- src/main/java/com/github/curriculeon/jfoot/Wombat.java
+++ src/main/java/com/github/curriculeon/jfoot/Wombat.java
@@ -1,8 +1,11 @@
 package com.github.curriculeon.jfoot;
 
+
 import com.github.git_leon.RandomUtils;
 
+
 import static com.github.curriculeon.jfoot.Direction.*;
+
 
 /**
  * Wombat. A Wombat moves forward until it hits the edge of the world, at
@@ -16,15 +19,64 @@ public class Wombat extends Herbivore {
         setImage("wombat.png");
     }
 
+
     public void act() {
-        if (this.foundLeaf()) {
+        if (this.foundLeaf()) { // if leaf found eat it.
             this.eatLeaf();
         } else if (this.canMove()) {
-            this.move();
+            this.move(); // if no leaf found move
         } else {
-            this.turnLeft();
+            if (getDirection() == EAST){
+                this.turnLeft(); // if no leaf and can move, turn left.
+                this.move();
+                this.turnLeft();
+            } else {
+                if (getDirection() == WEST)
+                    turnRight();
+                this.move();
+                turnRight();
+            }
         }
     }
+
+
+// public void turnRight(){
+// turnLeft();
+// turnLeft();
+// turnLeft();
+// }
+
+
+    private boolean isAtTopLeft() {
+        return !canMove(WEST) && !canMove(NORTH);
+    }
+
+
+    private boolean canMove(Direction direction){
+        Direction originalDirection = getDirection();
+        setDirection(direction);
+        boolean canMove = canMove();
+        setDirection(originalDirection);
+        return canMove;
+    }
+
+
+
+
+    public void turnRight() {
+        if (this.getDirection() == WEST) {
+            this.setDirection(NORTH);
+        } else if (this.getDirection() == EAST) {
+            this.setDirection(SOUTH);
+        } else if (this.getDirection() == NORTH) {
+            this.setDirection(EAST);
+        } else {
+            this.setDirection(WEST);
+        }
+    }
+
+
+
 
     public void turnLeft() {
         if (this.getDirection() == EAST) {
@@ -38,3 +90,4 @@ public class Wombat extends Herbivore {
         }
     }
 }
+
